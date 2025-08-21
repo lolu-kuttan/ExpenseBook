47c786844d52e133ae58e50ee182299f1c45f345
Sharan R <sharanrs1205@gmail.com>
2025-08-21 20:45:36 +0530

    Update View.java

--- Details/Expense_list/View/View.java
+++ Details/Expense_list/View/View.java
@@ -1,4 +1,4 @@
-  private void loadExpenses() {
+private void loadExpenses() {
         String sql = "SELECT * FROM expenses";
 
         try (Connection conn = DatabaseHelper.getConnection();
@@ -21,4 +21,39 @@
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private Callback<TableColumn<Expense, Void>, TableCell<Expense, Void>> getActionCellFactory() {
+        return new Callback<>() {
+            @Override
+            public TableCell<Expense, Void> call(final TableColumn<Expense, Void> param) {
+                return new TableCell<>() {
+                    private final Button editButton = new Button("Edit");
+                    private final Button deleteButton = new Button("Delete");
+                    private final HBox pane = new HBox(5, editButton, deleteButton);
+
+                    {
+                        editButton.setOnAction(e -> {
+                            Expense expense = getTableView().getItems().get(getIndex());
+                            showEditDialog(expense);
+                        });
+
+                        deleteButton.setOnAction(e -> {
+                            Expense expense = getTableView().getItems().get(getIndex());
+                            deleteExpense(expense);
+                        });
+                    }
+
+                    @Override
+                    public void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            setGraphic(pane);
+                        }
+                    }
+                };
+            }
+        };
     }
